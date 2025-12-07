788dee886a0c2e05874b364635cbb624e1c7ff66
claravanstaden <claravanstaden64@gmail.com>
2025-12-07 14:54:23 +0200

    remove descriptions

--- lib/wallets.ts
+++ lib/wallets.ts
@@ -1,7 +1,6 @@
 export interface WalletConfig {
   id: string;
   name: string;
-  description: string;
   extensionName: string;
   installUrl: string;
   icon: string;
@@ -11,7 +10,6 @@ export const SUPPORTED_WALLETS: WalletConfig[] = [
   {
     id: 'polkadot-js',
     name: 'Polkadot.js',
-    description: 'The original Polkadot wallet extension',
     extensionName: 'polkadot-js',
     installUrl: 'https://polkadot.js.org/extension/',
     icon: '/icons/polkadotjs.png',
@@ -19,7 +17,6 @@ export const SUPPORTED_WALLETS: WalletConfig[] = [
   {
     id: 'talisman',
     name: 'Talisman',
-    description: 'A better wallet for Polkadot & Ethereum',
     extensionName: 'talisman',
     installUrl: 'https://talisman.xyz/',
     icon: '/icons/talisman.png',
@@ -27,7 +24,6 @@ export const SUPPORTED_WALLETS: WalletConfig[] = [
   {
     id: 'subwallet',
     name: 'SubWallet',
-    description: 'The comprehensive Polkadot wallet',
     extensionName: 'subwallet-js',
     installUrl: 'https://subwallet.app/',
     icon: '/icons/subwallet.jpeg',
@@ -35,7 +31,6 @@ export const SUPPORTED_WALLETS: WalletConfig[] = [
   {
     id: 'nova',
     name: 'Nova Wallet',
-    description: 'Next-gen wallet for Polkadot ecosystem',
     extensionName: 'polkadot-js',
     installUrl: 'https://novawallet.io/',
     icon: '/icons/nova.png',
